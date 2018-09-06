a125993480ed8f135daeb5488d84086ad8f6eff8
mkravchuk <kerey07@gmail.com>
2018-09-06 21:49:23 +0300

    1)Исправлен вывод массива. 2)Исправленна нумерация четвертей (см. четверти координатной плоскости)

--- task3.py
+++ task3.py
@@ -11,25 +11,30 @@ L_x = L_x - 1
 
 array = np.random.randint(2, size=(5, 5))  # создаю массив
 
-print(array)  # вывод массива
+for i in range(5):
+    for ii in range(5):
+        print(array[i, ii], end='  ')
+        if ii == 4:
+            print('\n')
+
 
 for counter in range(4):
     x = 0
     y = 0
     quarter_sum = 0
-    if counter == 0:
+    if counter == 1:
         for y in range(K_y):
             for x in range(L_x):
                 quarter_sum = quarter_sum + array[y, x]
         answer_list.append(quarter_sum)
-    elif counter == 1:
+    elif counter == 0:
         for y in range(K_y):
             x = L_x + 1
             while x < 5:
                 quarter_sum = quarter_sum + array[y, x]
                 x += 1
         answer_list.append(quarter_sum)
-    elif counter == 2:
+    elif counter == 3:
         y = K_y + 1
         while y < 5:
             x = L_x + 1
@@ -38,7 +43,7 @@ for counter in range(4):
                 x += 1
             y += 1
         answer_list.append(quarter_sum)
-    elif counter == 3:
+    elif counter == 2:
         y = K_y + 1
         while y < 5:
             for x in range(L_x):
